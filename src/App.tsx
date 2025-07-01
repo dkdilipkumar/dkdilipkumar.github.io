63fa940817e107a6106854020fdba58770715588
Dilip Kumar <dilip.kumar.96995@gmail.com>
2025-07-01 11:42:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import NotFound from "./pages/AppAds";
 
 const queryClient = new QueryClient();
 
@@ -19,7 +18,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Route path="/app-ads.txt" element={<AppAds />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
